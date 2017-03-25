5f9895fc9047673080a7027c951d583219f3b61f
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-03-25 10:41:06 -0600

    sign up, login, create/join, create completed

--- js/app.js
+++ js/app.js
@@ -5,6 +5,26 @@ angular.module('personalProj', ['ui.router'])
             url: '/',
             templateUrl: 'views/landing.html'
           })
+          .state('signup', {
+            url: '/signup',
+            templateUrl: 'views/signup.html'
+          })
+          .state('login', {
+            url: '/login',
+            templateUrl: 'views/login.html'
+          })
+          .state('createorjoin', {
+            url: '/createorjoin',
+            templateUrl: 'views/createorjoin.html'
+          })
+          .state('creategroup', {
+            url: '/creategroup',
+            templateUrl: 'views/creategroup.html'
+          })
+          .state('joingroup', {
+            url: '/joingroup',
+            templateUrl: 'views/joingroup.html'
+          })
 
           $urlRouterProvider
       .otherwise('/');
